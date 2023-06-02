703b900d8f64e7502e117de5fcc816f7a33ff0f1
NicholasPilotto <nicholaspilotto@gmail.com>
2023-06-02 18:12:31 +0200

    Project init

---
+++ Cleanify/Cleanify/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Cleanify
+//
+//  Created by Nicholas Pilotto on 02/06/23.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
